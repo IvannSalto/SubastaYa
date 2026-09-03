ef2943e524009fee179ab8f2fbadb93a3e1ea201
GeiceLeonel <leonelgeice1@gmail.com>
2026-09-03 10:46:18 -0300

    implemento interfaz de servicio auditlog

--- SubastaYa.Core/Interfaces/IAuditLog.cs
+++ SubastaYa.Core/Interfaces/IAuditLog.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using SubastaYa.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SubastaYa.Core.Interfaces
 {
-    internal interface IAuditLog
+    public interface IAuditLog
     {
+        // registrar un evento en el log de auditoría
+        Task LogAsync(string entity, string entityId, string action, int? userId, string? details);
+
+        // Consultar eventos auditados para reportes o panel de control
+        Task<IEnumerable<AuditLog>> GetLogsByEntityAsync(string entity, string entityId);
     }
 }
