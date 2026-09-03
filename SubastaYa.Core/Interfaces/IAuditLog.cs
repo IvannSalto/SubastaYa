63b61e0ecd2f196afc7449e746a0edf8e9e6b7e9
GeiceLeonel <leonelgeice1@gmail.com>
2026-09-03 10:34:37 -0300

    impento la interfaz de servicio de subasta

---
+++ SubastaYa.Core/Interfaces/IAuditLog.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubastaYa.Core.Interfaces
+{
+    internal interface IAuditLog
+    {
+    }
+}
--- SubastaYa.Infrastructure/Repositories/GenericRepository.cs
+++ SubastaYa.Infrastructure/Repositories/GenericRepository.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using SubastaYa.Core.IRepositories;
+using SubastaYa.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SubastaYa.Infrastructure.Repositories
 {
-    internal class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        protected readonly ApplicationDbContext _context;   
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public void Update(T entity) => _context.Set<T>().Update(entity);
+
+        public void Delete(T entity) => _context.Set<T>().Remove(entity);
     }
 }
