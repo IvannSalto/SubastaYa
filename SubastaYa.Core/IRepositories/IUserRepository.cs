1b24732f33b1cce50a2fb0310ec9eee08e155d36
GeiceLeonel <leonelgeice1@gmail.com>
2026-09-05 14:36:41 -0300

    Agrego clase e interface repository de user

---
+++ SubastaYa.Core/IRepositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubastaYa.Core.IRepositories
+{
+    internal class IUserRepository
+    {
+    }
+}
---
+++ SubastaYa.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubastaYa.Infrastructure.Repositories
+{
+    internal class UserRepository
+    {
+    }
+}
