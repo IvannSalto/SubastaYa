760ca47bed13105cbd2ec10f30f574268d945235
GeiceLeonel <leonelgeice1@gmail.com>
2026-09-08 17:14:24 -0300

    Agrego hubs y signalr

---
+++ SubastaYa.Api/Hubs/AuctionHubs.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SubastaYa.Api.Hubs
+{
+    public class AuctionHub : Hub
+    {
+        // Acá el Frontend se va a suscribir a la "sala" de una subasta específica
+        public async Task JoinAuctionGroup(string auctionId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, auctionId);
+        }
+    }
+}
